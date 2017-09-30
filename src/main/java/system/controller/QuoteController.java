fd6fd864cb40bd20b5caa7d3000961a0e5897420
MxWild <mxwild@gmail.com>
2017-09-30 23:03:39 +0500

    Added Controller

--- src/main/java/system/controller/QuoteController.java
+++ src/main/java/system/controller/QuoteController.java
@@ -1,28 +1,73 @@
 package system.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 import system.model.Quote;
 import system.service.QuoteService;
-
-import java.util.List;
-import java.util.Map;
 
 @Controller
 //@RequestMapping("/quotes")
 public class QuoteController {
 
+    private QuoteService quoteService;
+
     @Autowired
+    @Qualifier(value = "QuoteService")
+    public void setQuoteService(QuoteService quoteService) {
+        this.quoteService = quoteService;
+    }
+
+    @RequestMapping(value = "quotes", method = RequestMethod.GET)
+    public String listQuotes(Model model) {
+        model.addAttribute("quote", new Quote());
+        model.addAttribute("listQuotes", this.quoteService.listAllQuotes());
+
+        return "quotes";
+    }
+
+    @RequestMapping(value = "/quotes/add", method = RequestMethod.POST)
+    public String addQuote(@ModelAttribute("quote") Quote quote) {
+        if (quote.getId() == 0) this.quoteService.addQuote(quote);
+        else this.quoteService.updateQuote(quote);
+
+        return "redirect:/quotes";
+    }
+
+    @RequestMapping("/remove/{id}")
+    public String removeQuote(@PathVariable("id")  int id){
+        this.quoteService.removeQuote(id);
+
+        return "redirect:/quotes";
+    }
+
+    @RequestMapping("/edit/{id}")
+    public String editQuote(@PathVariable("id") int id, Model model) {
+        model.addAttribute("Quote", this.quoteService.getQuoteById(id));
+        model.addAttribute("listQuotes", this.quoteService.listAllQuotes());
+
+        return "quotes";
+    }
+
+    @RequestMapping("quotedata/{id}")
+    public String quoteData(@PathVariable("id") int id, Model model) {
+        model.addAttribute("quote", this.quoteService.getQuoteById(id));
+
+        return "quotedata";
+    }
+
+    /*    @Autowired
     private QuoteService quoteService;
 
     @RequestMapping(value = "/all", method = RequestMethod.GET)
     private @ResponseBody List<Quote> getAllQuotas() {
         return quoteService.getAllQuotes();
-    }
+    }*/
 
  /*   @RequestMapping(value = "/all", method = RequestMethod.GET)
     public ModelAndView getAllQuotas() {
--- src/main/webapp/WEB-INF/pages/quotas_page.jsp
+++ src/main/webapp/WEB-INF/pages/quotas_page.jsp
@@ -1,7 +1,28 @@
+<%--<%@ page contentType="text/html;charset=UTF-8" language="java" %>--%>
+<%--<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>--%>
+<%--<jsp:include page ="inc/header.jsp" />--%>
+<%--<hr>--%>
+    <%--<h1>This is Quotas Page</h1>--%>
+<%--<hr>--%>
+<%--<jsp:include page="inc/footer.jsp"/>--%>
+
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<jsp:include page ="inc/header.jsp" />
-<hr>
-    <h1>This is Quotas Page</h1>
-<hr>
-<jsp:include page="inc/footer.jsp"/>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel='stylesheet' href='/webjars/bootstrap/css/bootstrap.min.css'>
+    <script src="/webjars/jquery/1.11.1/jquery.min.js"></script>
+    <script src="/webjars/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>Quotes</title>
+</head>
+<body>
+    <h1>Quotes List</h1>
+    <hr>
+
+    <a href="/quotes" target="_blank">Quotes List</a>
+
+    <hr>
+</body>
+</html>
