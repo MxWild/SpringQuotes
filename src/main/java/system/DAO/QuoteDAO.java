83be4e1d638b7e63df1dd4fa9475276a3c7d17d0
MxWild <mxwild@gmail.com>
2017-09-30 22:02:38 +0500

    Create interface DAO

--- src/main/java/system/DAO/QuoteDAO.java
+++ src/main/java/system/DAO/QuoteDAO.java
@@ -1,36 +1,4 @@
 package system.DAO;
 
-import org.springframework.stereotype.Repository;
-import system.model.Author;
-import system.model.Category;
-import system.model.Quote;
-
-import java.util.Arrays;
-import java.util.List;
-
-@Repository
-public class QuoteDAO {
-
-    private List<Quote> quotesList = Arrays.asList(new Quote("Истинно любит тебя тот, кто втайне молится о тебе Богу.",
-                                                            "Николай Сербский",
-                                                            "Великие Слова"),
-                                                    new Quote("Люди злы в той мере, в какой несчастны.",
-                                                            "Уильям Сомерсет Моэм",
-                                                            "Великие Слова"));
-
-    // Add Quote
-    public void addQoute(String text, Author author, Category category) {
-
-    }
-
-    // Remove Quote
-    public void removeQuote(Integer id) {
-
-    }
-
-    // List all Quotes
-    public List<Quote> getListQuotes() {
-        return quotesList;
-    }
-
+public interface QuoteDAO {
 }
